b1bd777e0cc003fe8e4cea838eb341b333852852
JuanManuelParedes <juanmaparedesb@gmail.com>
2026-05-27 22:12:17 -0500

    Update EcoMision.h con documentacion

--- src/model/EcoMision.h
+++ src/model/EcoMision.h
@@ -16,22 +16,22 @@ public:
 
     ~EcoMision();
 
-
+    // Punto de entrada principal del programa
     void iniciar();
 
-
+    // Crea la reserva y registra todas las zonas con sus elementos
     void prepararReserva();
 
-
+    // Crea al explorador y lo ubica en la zona inicial
     void crearExplorador();
 
-
+    // Ejecuta una secuencia de interacciones de demostración
     void ejecutarDemostracion();
 
-
+    // Muestra el menu
     void mostrarMenu ();
 
-
+    // Muestra el menu de configuracion personalizada
     void menuConfiguracion();
 };
 
